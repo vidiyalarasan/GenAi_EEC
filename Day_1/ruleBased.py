518f036d2b34d2eda1913b6eb93b190ae31ca00b
vidiyalarasan <vidiyalarasans@gmail.com>
2026-02-09 13:55:28 +0530

    ruleBased.py Fixed

--- Day_1/ruleBased.py
+++ Day_1/ruleBased.py
@@ -1,5 +1,7 @@
 def input_validation(text):
     return text.strip().lower()
+
+
 def intent_detection(text):
     if "price" in text:
         return "price"
@@ -8,12 +10,35 @@ def intent_detection(text):
     elif "bye" in text:
         return "exit"
     else:
-        return:"unknown"
-def knowledege_base(text):
-    data={
-        "price":"product price $100",
-        "greet":"how can i help you","exit":"Have a good day",
-        "unknown":"i dont understand"
-        }
-return data[intent_detection]
-        
+        return "unknown"
+
+
+def knowledge_base(intent):
+    data = {
+        "price": "Product price is $100",
+        "greet": "How can I help you?",
+        "exit": "Have a good day",
+        "unknown": "Sorry, I don't understand that"
+    }
+    return data[intent]
+
+
+def ai_processing(response):
+    return response
+
+
+def chatbot_workflow(user_input):
+    step1 = input_validation(user_input)
+    step2 = intent_detection(step1)
+    step3 = knowledge_base(step2)
+    step4 = ai_processing(step3)
+    return step4
+
+
+while True:
+    user = input("You: ")
+    bot = chatbot_workflow(user)
+    print("Bot:", bot)
+
+    if "bye" in user.lower():
+        break
